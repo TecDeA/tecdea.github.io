c48afe9a31d3b711497616a78f20730ea3901053
angelmicelti <angelmicelti@users.noreply.github.com>
2026-08-31 21:45:50 +0200

    Revert "Update sw.js"
    This reverts commit 41ce20de94d42037b902e829ab7f579d383e8a3e.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 //  SERVICE WORKER - TECDEA PORTAL PWA
 // ════════════════════════════════════════════════════════
 
-const CACHE_NAME = 'tecdea-portal-v29';
+const CACHE_NAME = 'tecdea-portal-v28';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
