79f7f78536e980169cf82be2cfd2e182d7ddc055
angelmicelti <angelmicelti@users.noreply.github.com>
2026-09-02 17:47:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 //  SERVICE WORKER - TECDEA PORTAL PWA
 // ════════════════════════════════════════════════════════
 
-const CACHE_NAME = 'tecdea-portal-v31';
+const CACHE_NAME = 'tecdea-portal-v32';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
